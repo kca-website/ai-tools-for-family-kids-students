774fcf494f0e58bad03f08437cde8aefc2b6f537
kca-website <kkoustas@gmail.com>
2026-09-24 17:25:27 +0300

    Load ENEEGYL core study routes in tutor

--- special-education-tutor-ui.js
+++ special-education-tutor-ui.js
@@ -46,6 +46,7 @@
     ["eneegyl-physics-2026-2027","/teacher-curriculum-eneegyl-physics-2026-2027.js"],
     ["eneegyl-new-greek-2026-2027","/teacher-curriculum-eneegyl-new-greek-2026-2027.js"],
     ["eneegyl-english-2026-2027","/teacher-curriculum-eneegyl-english-2026-2027.js"],
+    ["special-education-eneegyl-core-learning","/special-education-eneegyl-core-learning-2026-2027.js"],
     ["special-education-tutor-context","/special-education-tutor-context.js"],
     ["special-education-tutor-catalog","/special-education-tutor-catalog.js"]
   ];
